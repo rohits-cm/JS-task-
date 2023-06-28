48a0eccea533d8326c1a38867890aa6075e2109c
rohits-cm <132444176+rohits-cm@users.noreply.github.com>
2023-06-28 17:59:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 let firstname = "Rohit";
 let username = window.prompt("What is your name?");
-// let age=24;
 let age= window.prompt("What is your age?");
 age = Number(age);
 age += 1;
-// let student = true;
 
-// age= age+1;
-
-// console.log(firstname);
-// console.log("you will be", age , " years old");
-// console.log("Enrolled:",student);
 document.getElementById("p1").innerHTML = "Hello " + username;
 document.getElementById("p2").innerHTML = "You will be " + age + " years old";
-// document.getElementById("p3").innerHTML = "Enrolled: " + student;
 
 console.log(username);
